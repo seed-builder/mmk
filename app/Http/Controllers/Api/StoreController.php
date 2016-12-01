0707cf40f8a1c7dadfc5a99a2c022e825a1f11f4
gsjohn <sz.gs.john@gmail.com>
2016-12-01 19:59:20 +0800

    change field type

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -25,6 +25,29 @@ class StoreController extends ApiController
         return response($entity, 200);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = $request->all();
+        if(!empty($data['fpostalcode'])){
+            $count = Store::where('fpostalcode', $data['fpostalcode'])->count();
+            $data['fnumber'] = $data['fpostalcode'] . ($count+1);
+        }
+
+        unset($data['_sign']);
+        $entity = $this->newEntity($data);
+        //$entity = Entity::create($data);
+        $re = $entity->save();
+        $status = $re ? 200 : 400;
+        return response($entity, $status);
+    }
+
     public function newEntity(array $attributes = [])
     {
         // TODO: Implement newEntity() method.
