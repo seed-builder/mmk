5ae7f7091811121f533db523d9d2d45921a302e9
gsjohn <sz.gs.john@gmail.com>
2016-12-17 19:21:06 +0800

    bug fix

--- app/Models/Busi/WorkCalendar.php
+++ app/Models/Busi/WorkCalendar.php
@@ -4,7 +4,8 @@ namespace App\Models\Busi;
 
 use Illuminate\Database\Eloquent\Model;
 
-class WorkCalendar extends Model
+class WorkCalendar extends BaseModel
 {
     //
+	protected $table = 'eng_work_calendar';
 }
--- app/Models/Busi/WorkCalendarData.php
+++ app/Models/Busi/WorkCalendarData.php
@@ -4,7 +4,8 @@ namespace App\Models\Busi;
 
 use Illuminate\Database\Eloquent\Model;
 
-class WorkCalendarData extends Model
+class WorkCalendarData extends BaseModel
 {
     //
+	protected $table = 'eng_work_calendar_data';
 }
