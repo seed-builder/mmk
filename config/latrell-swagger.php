a28b20d8976740e820292d4f0e8a0444a70aa04c
gsjohn <sz.gs.john@gmail.com>
2017-01-04 17:53:06 +0800

    add test  login employee data

--- config/latrell-swagger.php
+++ config/latrell-swagger.php
@@ -7,7 +7,7 @@ return array(
 	'paths' => [base_path('app'), base_path('routes')],
 	'output' => storage_path('swagger/docs'),
 	'exclude' => null,
-	'default-base-path' => env('SWAGGER_BASE_PATH'),
+	'default-base-path' => '/', //env('SWAGGER_BASE_PATH'),
 	'default-api-version' => null,
 	'default-swagger-version' => null,
 	'api-doc-template' => null, //
