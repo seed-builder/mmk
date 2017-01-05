fda2643d4cda810b09a1cacb8f48efbadb92f355
gsjohn <sz.gs.john@gmail.com>
2017-01-05 09:47:01 +0800

    add test  login employee data

--- app/Http/Controllers/Api/VisitLineCalendarController.php
+++ app/Http/Controllers/Api/VisitLineCalendarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Busi\VisitLineCalendar;
+use DB;
 
 class VisitLineCalendarController extends ApiController
 {
