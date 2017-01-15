ddc0faf511088a822f8e5f3687aaf69ace0e7468
gsjohn <sz.gs.john@gmail.com>
2017-01-16 00:27:25 +0800

    auth

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
