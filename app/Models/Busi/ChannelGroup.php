e988ae29a08eb06e3b045eb9b9a2186018991d25
gsjohn <sz.gs.john@gmail.com>
2017-01-17 16:47:00 +0800

    channel group props add children count and channels count

--- app/Models/Busi/ChannelGroup.php
+++ app/Models/Busi/ChannelGroup.php
@@ -3,6 +3,7 @@
 namespace App\Models\Busi;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Busi\Channel;
 
 /**
  * model description
@@ -11,16 +12,18 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @author  xrs
  * @SWG\Model(id="ChannelGroup")
- * @SWG\Property(name="fcreate_date", type="string", description="创建时间")
- * @SWG\Property(name="fcreator_id", type="integer", description="创建人")
- * @SWG\Property(name="fdocument_status", type="string", description="数据状态")
- * @SWG\Property(name="fmodify_date", type="string", description="修改时间")
- * @SWG\Property(name="fmodify_id", type="integer", description="修改人")
  * @SWG\Property(name="fname", type="string", description="分组名称")
  * @SWG\Property(name="fnumber", type="string", description="分组编码")
  * @SWG\Property(name="fparent_id", type="integer", description="分组上级")
  * @SWG\Property(name="fsort", type="integer", description="排序")
  * @SWG\Property(name="ftype", type="string", description="分组类型")
+ * @SWG\Property(name="children_count", type="integer", description="子分组数量")
+ * @SWG\Property(name="channel_count", type="integer", description="渠道数量")
+ * @SWG\Property(name="fcreate_date", type="string", description="创建时间")
+ * @SWG\Property(name="fcreator_id", type="integer", description="创建人")
+ * @SWG\Property(name="fdocument_status", type="string", description="数据状态")
+ * @SWG\Property(name="fmodify_date", type="string", description="修改时间")
+ * @SWG\Property(name="fmodify_id", type="integer", description="修改人")
  * @SWG\Property(name="id", type="integer", description="")
   */
 class ChannelGroup extends BaseModel
@@ -28,4 +31,20 @@ class ChannelGroup extends BaseModel
 	//
 	protected $table = 'bd_channel_groups';
 	protected $guarded = ['id'];
+	protected $casts = [
+		'children_count' => 'integer',
+		'channel_count' => 'integer',
+	];
+	protected $visible = ['id', 'fnumber', 'fname', 'fparent_id', 'fsort', 'children_count', 'channel_count'];
+	protected $appends = ['children_count', 'channel_count'];
+
+	public function getChildrenCountAttribute(){
+		$c = static::where('fparent_id', $this->id)->count();
+		return $c;
+	}
+
+	public function getChannelCountAttribute(){
+		$c = Channel::where('fgroup_id', $this->id)->count();
+		return $c;
+	}
 }
