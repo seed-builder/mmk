4f603a09cbefc9e2da8f6fef5596a9a46538377f
gsjohn <sz.gs.john@gmail.com>
2017-01-18 11:56:11 +0800

    bug fix

--- app/Http/Controllers/Api/AppUpgradeController.php
+++ app/Http/Controllers/Api/AppUpgradeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Api\ApiController;
 use App\Models\Busi\AppUpgrade;
 
 class AppUpgradeController extends ApiController
