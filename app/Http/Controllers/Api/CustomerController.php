43f32dc2700e98195b10e742762932cf7f2089ae
gsjohn <sz.gs.john@gmail.com>
2017-02-21 16:32:53 +0800

    data isolate

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Busi\Employee;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Busi\Customer;
--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Busi\Employee;
 use App\Models\City;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
