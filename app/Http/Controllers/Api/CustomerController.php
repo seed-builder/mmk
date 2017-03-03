c91fb4817ac548c01418334d48604751db7bd442
gsjohn <sz.gs.john@gmail.com>
2017-03-03 22:53:13 +0800

    bug fix

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Busi\Employee;
+use App\Repositories\ISysConfigRepo;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Busi\Customer;
