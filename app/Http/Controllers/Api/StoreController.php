c8c5a340132c2b3a469013e2eec1a251b79cd9a5
gsjohn <sz.gs.john@gmail.com>
2017-03-03 22:46:23 +0800

    bug fix

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Busi\Employee;
 use App\Models\City;
+use App\Repositories\ISysConfigRepo;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Busi\Store;
