658c8cc40e0b88b90291384e03e1f6ae50e5bc02
gsjohn <sz.gs.john@gmail.com>
2017-03-06 12:11:52 +0800

    bug fix

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Repositories\SysConfigRepo;
+use SysConfigRepo;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
