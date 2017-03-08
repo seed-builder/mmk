9e972858b6cf1992a3031c03bccd56c6cd73be45
gsjohn <sz.gs.john@gmail.com>
2017-03-08 15:49:51 +0800

    sale order , stock in out

---
+++ database/migrations/2017_03_08_152652_create_table_st_stock_ins.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableStStockIns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('st_stock_ins', function (Blueprint $table) {
+            $table->increments('id');
+	        $table->string('fbill_no')->default('')->comment('订单单号(门店编码+日期)');
+	        $table->timestamp('fsend_date')->nullable()->comment('发货日期');
+	        $table->timestamp('fin_date')->nullable()->comment('到货日期');
+	        $table->integer('fcust_id')->default(0)->comment('经销商ID');
+	        $table->char('fsend_status', 1)->default('A')->comment('发货状态(A-未发货，B-发货中，C-已到货)');
+	        $table->integer('fuser_id')->default(0)->comment('到货确认人id');
+
+	        $table->integer('fcreator_id')->default(0)->comment('创建人');
+	        $table->timestamp('fcreate_date')->nullable()->comment('创建时间');
+	        $table->integer('fmodify_id')->default(0)->comment('修改人');
+	        $table->timestamp('fmodify_date')->nullable()->comment('修改时间');
+	        $table->string('fdocument_status')->default('A')->comment('审核状态');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('st_stock_ins');
+    }
+}
