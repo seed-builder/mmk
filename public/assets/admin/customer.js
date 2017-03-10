2d4a121c8674c4f00b5f978e2e878369de5116a8
shinerain <529546542@qq.com>
2017-03-10 21:30:47 +0800

    代码优化

--- public/assets/admin/customer.js
+++ public/assets/admin/customer.js
@@ -1,7 +1,7 @@
 /**
-*
-*/
-define(function(require, exports, module) {
+ *
+ */
+define(function (require, exports, module) {
 
     var zhCN = require('datatableZh');
     var editorCN = require('i18n');
@@ -28,45 +28,34 @@ define(function(require, exports, module) {
             table: "#" + tableId,
             idSrc: 'id',
             fields: [
-            { 'label':  'faddress', 'name': 'faddress', },
-                { 'label':  'farea', 'name': 'farea', },
-                { 'label':  'fauditor_id', 'name': 'fauditor_id', },
-                { 'label':  'faudit_date', 'name': 'faudit_date', },
-                { 'label':  'fbusiness_mode', 'name': 'fbusiness_mode', },
-                { 'label':  'fcity', 'name': 'fcity', },
-                { 'label':  'fcompany_nature', 'name': 'fcompany_nature', },
-                { 'label':  'fcompany_scale', 'name': 'fcompany_scale', },
-                { 'label':  'fcountry', 'name': 'fcountry', },
-                { 'label':  'fcreate_date', 'name': 'fcreate_date', },
-                { 'label':  'fcreator_id', 'name': 'fcreator_id', },
-                { 'label':  'fcust_type_id', 'name': 'fcust_type_id', },
-                { 'label':  'fdiscount_list_id', 'name': 'fdiscount_list_id', },
-                { 'label':  'fdocument_status', 'name': 'fdocument_status', },
-                { 'label':  'ffax', 'name': 'ffax', },
-                { 'label':  'fforbidder_id', 'name': 'fforbidder_id', },
-                { 'label':  'fforbid_date', 'name': 'fforbid_date', },
-                { 'label':  'fforbid_status', 'name': 'fforbid_status', },
-                { 'label':  'fgroup', 'name': 'fgroup', },
-                { 'label':  'finvoice_type', 'name': 'finvoice_type', },
-                { 'label':  'fis_credit_check', 'name': 'fis_credit_check', },
-                { 'label':  'fmode_transport', 'name': 'fmode_transport', },
-                { 'label':  'fmodify_date', 'name': 'fmodify_date', },
-                { 'label':  'fmodify_id', 'name': 'fmodify_id', },
-                { 'label':  'fname', 'name': 'fname', },
-                { 'label':  'fprice_list_id', 'name': 'fprice_list_id', },
-                { 'label':  'fprovince', 'name': 'fprovince', },
-                { 'label':  'fsale_depart', 'name': 'fsale_depart', },
-                { 'label':  'fseller', 'name': 'fseller', },
-                { 'label':  'fservice_depart', 'name': 'fservice_depart', },
-                { 'label':  'fshort_name', 'name': 'fshort_name', },
-                { 'label':  'ftax_rate', 'name': 'ftax_rate', },
-                { 'label':  'ftax_register_code', 'name': 'ftax_register_code', },
-                { 'label':  'ftax_type', 'name': 'ftax_type', },
-                { 'label':  'ftel', 'name': 'ftel', },
-                { 'label':  'ftrading_curr_id', 'name': 'ftrading_curr_id', },
-                { 'label':  'fwebsite', 'name': 'fwebsite', },
-                { 'label':  'fzip', 'name': 'fzip', },
-        ]
+                {'label': '通讯地址', 'name': 'faddress',},
+                {'label': '地区', 'name': 'farea',},
+                {'label': '经营模式', 'name': 'fbusiness_mode',},
+                {'label': '城市', 'name': 'fcity',},
+                {'label': '公司性质', 'name': 'fcompany_nature',},
+                {'label': '公司规模', 'name': 'fcompany_scale',},
+                {'label': '国家', 'name': 'fcountry',},
+                {'label': '客户类别', 'name': 'fcust_type_id',},
+                {'label': '折扣表', 'name': 'fdiscount_list_id',},
+                {'label': '传真', 'name': 'ffax',},
+                {'label': '客户分组', 'name': 'fgroup',},
+                {'label': '发票类型', 'name': 'finvoice_type',},
+                {'label': '运输方式', 'name': 'fmode_transport',},
+                {'label': '客户名称', 'name': 'fname',},
+                {'label': '价目表', 'name': 'fprice_list_id',},
+                {'label': '省份', 'name': 'fprovince',},
+                {'label': '所属服务处', 'name': 'fsale_depart',},
+                {'label': '销售员', 'name': 'fseller',},
+                {'label': '所属营业部', 'name': 'fservice_depart',},
+                {'label': '客户简称', 'name': 'fshort_name',},
+                {'label': '默认税率', 'name': 'ftax_rate',},
+                {'label': '纳税登记号', 'name': 'ftax_register_code',},
+                {'label': '税分类', 'name': 'ftax_type',},
+                {'label': '联系电话', 'name': 'ftel',},
+                {'label': '结算币别', 'name': 'ftrading_curr_id',},
+                {'label': '公司网址', 'name': 'fwebsite',},
+                {'label': '邮政编码', 'name': 'fzip',},
+            ]
         });
 
         var table = $("#" + tableId).DataTable({
@@ -79,10 +68,10 @@ define(function(require, exports, module) {
             rowId: "id",
             ajax: '/admin/customer/pagination',
             columns: [
-                {  'data': 'id' },
-                {  'data': 'fname' },
-                {  'data': 'faddress' },
-                {  'data': 'ftel' },
+                {'data': 'id'},
+                {'data': 'fname'},
+                {'data': 'faddress'},
+                {'data': 'ftel'},
             ],
             buttons: [
                 // { text: '新增', action: function () { }  },
