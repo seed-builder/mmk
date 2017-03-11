2aeab8d7bd15741f3aff3447767b1061299dd863
gsjohn <sz.gs.john@gmail.com>
2017-03-11 16:16:27 +0800

    bug fix

--- app/Http/Controllers/Api/VisitTodoCalendarController.php
+++ app/Http/Controllers/Api/VisitTodoCalendarController.php
@@ -47,11 +47,11 @@ class VisitTodoCalendarController extends ApiController
 				$storeCalendar = VisitStoreCalendar::find($conditions['fstore_calendar_id']);
 				if($storeCalendar->store->fis_signed){
 					$data = $data->reject(function($item){
-						return $item->todo->fname == '正常陈列';
+						return isset($item->todo) && $item->todo->fname == '正常陈列';
 					});
 				}else{
 					$data = $data->reject(function($item){
-						return $item->todo->fname == '陈列费用政策';
+						return isset($item->todo) &&  $item->todo->fname == '陈列费用政策';
 					});
 				}
 			}
