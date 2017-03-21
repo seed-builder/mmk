7f409e151dae179c950d0f7fed7a8759fa91fd27
shinerain <529546542@qq.com>
2017-03-21 16:27:11 +0800

    bug fix

--- app/Http/Controllers/Admin/StockController.php
+++ app/Http/Controllers/Admin/StockController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Admin\AdminController;
 use App\Models\Busi\Stock;
+use Illuminate\Support\Facades\Auth;
 
 class StockController extends AdminController
 {
@@ -70,4 +71,51 @@ class StockController extends AdminController
 		return parent::pagination($request, $searchCols, ['store.employee', 'material']);
 	}
 
+    /*
+     * 审核
+     */
+    public function check(Request $request){
+        $data = $request->all();
+        $ids = explode(",",$data['ids']);
+        $entitys = $this->newEntity()->newQuery()->whereIn('id',$ids)->get();
+
+        foreach ($entitys as $entity){
+            $entity->fdocument_status="C";
+            $entity->fcheck_type="B";
+            $entity->fcheck_date=date('Y-m-d H:i:s');
+            $entity->fchecker=Auth::user()->fname;
+
+            $entity->save();
+        }
+
+        return response()->json([
+            'code' => 200,
+            'result' => '审核成功！',
+            'data' => $entitys
+        ]);
+    }
+
+    /*
+     * 反审核
+     */
+    public function unCheck(Request $request){
+        $data = $request->all();
+        $ids = explode(",",$data['ids']);
+        $entitys = $this->newEntity()->newQuery()->whereIn('id',$ids)->get();
+
+        foreach ($entitys as $entity){
+            $entity->fdocument_status="A";
+            $entity->fcheck_type="B";
+            $entity->fcheck_date=date('Y-m-d H:i:s');
+            $entity->fchecker=Auth::user()->fname;
+
+            $entity->save();
+        }
+
+        return response()->json([
+            'code' => 200,
+            'result' => '反审核成功！',
+            'data' => $entitys
+        ]);
+    }
 }
