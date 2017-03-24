720f524737b1ed78c624ad9c0da21ce9672f5a2b
gsjohn <sz.gs.john@gmail.com>
2017-03-24 15:39:01 +0800

    bug fix

--- public/assets/admin/store.js
+++ public/assets/admin/store.js
@@ -325,9 +325,9 @@ define(function (require, exports, module) {
             attrs.push({"name":"地址","value":data.faddress})
             attrs.push({"name":"负责人","value":data.fcontracts})
             attrs.push({"name":"电话","value":data.ftelephone})
-            attrs.push({"name":"负责业代","value":data.employee.fname?data.employee.fname:''})
+            attrs.push({"name":"负责业代","value":data.employee && data.employee.fname?data.employee.fname:''})
             if(data.customer && data.customer.fname)
-                attrs.push({"name":"所属客户","value":data.customer.fname})
+                attrs.push({"name":"所属客户","value":data.customer.fname});
             var obj = {"title":data.ffullname,"attrs":attrs};
 
             mapWindow(element,obj);
