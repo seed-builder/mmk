56adaf64f67815c63dea8399f632adaf6656a91c
shinerain <529546542@qq.com>
2017-04-15 17:11:20 +0800

    密码重置

--- routes/admin/user.php
+++ routes/admin/user.php
@@ -1,5 +1,6 @@
 <?php
 Route::get('user/pagination', ['uses' => 'UserController@pagination']);
+Route::get('user/reset-pwd', ['uses' => 'UserController@resetPwd']);
 Route::match(['get', 'post'], 'user/{id}/set-role', ['uses' => 'UserController@setRole']);
 Route::match(['get', 'post'], 'user/{id}/set-position', ['uses' => 'UserController@setPosition']);
 Route::resource('user', 'UserController');
