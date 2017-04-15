952aa548e8ec8e02d626055fc5956c0ac714e87a
gsjohn <sz.gs.john@gmail.com>
2017-04-15 11:12:31 +0800

    bug fix

--- database/migrations/2017_04_13_121116_alter_table_bd_messages.php
+++ database/migrations/2017_04_13_121116_alter_table_bd_messages.php
@@ -16,6 +16,8 @@ class AlterTableBdMessages extends Migration
         Schema::table('bd_messages', function (Blueprint $table) {
             //
 	        $table->integer('type')->default(0)->comment('消息类型（0-系统公告,1-公司发文,2-流程消息,3-任务消息）');
+	        $table->integer('extra_id')->nullable()->comment('附加数据id');
+	        $table->string('extra_type')->nullable()->comment('附加数据类型');
         });
     }
 
@@ -29,6 +31,8 @@ class AlterTableBdMessages extends Migration
         Schema::table('bd_messages', function (Blueprint $table) {
             //
 	        $table->dropColumn('type');
+	        $table->dropColumn('extra_id');
+	        $table->dropColumn('extra_type');
         });
     }
 }
