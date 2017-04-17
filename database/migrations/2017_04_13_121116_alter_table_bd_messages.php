b3a77e4c3ee04b7115853a099c0f58d41a87d349
gsjohn <sz.gs.john@gmail.com>
2017-04-17 19:51:55 +0800

    bug fix

--- database/migrations/2017_04_13_121116_alter_table_bd_messages.php
+++ database/migrations/2017_04_13_121116_alter_table_bd_messages.php
@@ -18,8 +18,13 @@ class AlterTableBdMessages extends Migration
 	        $table->integer('type')->default(0)->comment('消息类型（0-系统公告,1-公司发文,2-流程消息,3-任务消息）');
 	        $table->integer('extra_id')->nullable()->comment('附加数据id');
 	        $table->string('extra_type')->nullable()->comment('附加数据类型');
-	        $table->string('subtitle')->nullable()->comment('副标题');
+
         });
+	    Schema::table('bd_message_contents', function (Blueprint $table) {
+		    //
+		    $table->integer('type')->default(0)->comment('消息类型（0-系统公告,1-公司发文,2-流程消息,3-任务消息）');
+		    $table->string('subtitle')->nullable()->comment('副标题');
+	    });
     }
 
     /**
@@ -34,7 +39,13 @@ class AlterTableBdMessages extends Migration
 	        $table->dropColumn('type');
 	        $table->dropColumn('extra_id');
 	        $table->dropColumn('extra_type');
-	        $table->dropColumn('subtitle');
+
         });
+
+	    Schema::table('bd_message_contents', function (Blueprint $table) {
+		    //
+		    $table->dropColumn('type');
+		    $table->dropColumn('subtitle');
+	    });
     }
 }
