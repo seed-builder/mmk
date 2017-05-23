700babdf67aded1da2b7809ab6547c1a7ba71041
gsjohn <sz.gs.john@gmail.com>
2017-05-23 22:09:47 +0800

    stock branch

--- app/Listeners/VisitDetailCompletedEventHandler.php
+++ app/Listeners/VisitDetailCompletedEventHandler.php
@@ -31,8 +31,25 @@ class VisitDetailCompletedEventHandler
         //
 		if($event->visit_todo_calendar_id){
 			$todoCalendar = VisitTodoCalendar::find($event->visit_todo_calendar_id);
-			$todoCalendar->fstatus = $this->completeStatus;
-			$todoCalendar->save();
+			$needDos = $this->checkEnd($todoCalendar);
+			if($needDos == 0) {
+				$todoCalendar->fstatus = $this->completeStatus;
+				$todoCalendar->save();
+			}
 		}
     }
+
+	public function checkEnd($todoCalendar){
+		$needDos = 0;
+		//拜访总结 - 结束
+		if(!empty($todoCalendar->todo) && $todoCalendar->todo->ffunction_number == 'JSBF'){
+			$needDos = VisitTodoCalendar::where('fstore_calendar_id', $todoCalendar->fstore_calendar_id)
+				->where('fparent_id', 0)
+				->where('fis_must_visit', 1)
+				->where('fstatus', '<', 3)
+				->where('id', '<>', $todoCalendar->id)
+				->count();
+		}
+		return $needDos;
+	}
 }
