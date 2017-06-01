1658c2d8e9b49e84deaa638fde622ba87e89d14c
gsjohn <sz.gs.john@gmail.com>
2017-06-01 21:24:41 +0800

    bug fix

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\ApiController;
-use App\Models\Busi\User;
+use App\Models\User;
 
 class UserController extends ApiController
 {
