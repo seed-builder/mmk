de460c360801250411090b8f09bb275828e1ab74
gsjohn <sz.gs.john@gmail.com>
2017-06-03 16:37:34 +0800

    bug fix

--- database/migrations/2017_06_03_152015_create_table_st_stock_checks.php
+++ database/migrations/2017_06_03_152015_create_table_st_stock_checks.php
@@ -28,12 +28,15 @@ class CreateTableStStockChecks extends Migration
 		    $table->increments('id');
 		    $table->integer("fstock_check_id");
 		    $table->integer("fmaterial_id");
-		    $table->decimal('finv_hqty')->default(0)->comment('期初库存余额箱数量（余额表取值）');
-		    $table->decimal('finv_eqty')->default(0)->comment('期初库存余额瓶数量（余额表取值）');
-		    $table->decimal('fcheck_hqty')->default(0)->comment('盘点箱数量');
-		    $table->decimal('fcheck_eqty')->default(0)->comment('盘点瓶数量');
-		    $table->decimal('fdiff_hqty')->default(0)->comment('盘点差异箱数量(库存减盘点)');
-		    $table->decimal('fdiff_eqty')->default(0)->comment('盘点差异瓶数量(库存减盘点)');
+		    $table->decimal('finv_hqty')->default(0)->comment('（合计）期初库存余额箱数量（余额表取值）');
+		    $table->decimal('finv_eqty')->default(0)->comment('（合计）期初库存余额瓶数量（余额表取值）');
+		    $table->decimal('fcheck_hqty')->default(0)->comment('（合计）盘点箱数量');
+		    $table->decimal('fcheck_eqty')->default(0)->comment('（合计）盘点瓶数量');
+		    $table->decimal('fdiff_hqty')->default(0)->comment('（合计）盘点差异箱数量(库存减盘点)');
+		    $table->decimal('fdiff_eqty')->default(0)->comment('（合计）盘点差异瓶数量(库存减盘点)');
+
+		    $table->decimal('box_qty')->default(0)->comment('（拆分）盘点箱数量');
+		    $table->decimal('bottle_qty')->default(0)->comment('（拆分）盘点瓶数量');
 
 		    $table->timestamp('fcreate_date')->nullable();
 		    $table->timestamp('fmodify_date')->nullable();
