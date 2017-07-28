334d78401792e8d6e00d8d9ab002af78233a984b
gsjohn <sz.gs.john@gmail.com>
2017-07-28 11:37:15 +0800

    bug fix

--- app/Http/Controllers/Api/VisitLineStoreController.php
+++ app/Http/Controllers/Api/VisitLineStoreController.php
@@ -13,4 +13,30 @@ class VisitLineStoreController extends ApiController
 		// TODO: Implement newEntity() method.
 		return new VisitLineStore($attributes);
 	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index(Request $request)
+	{
+		//
+		$page = $request->input('page', 1);
+		$pageSize = $request->input('pageSize', 10);
+		$sort = $request->input('sort', 'id asc');
+		$entity = $this->newEntity();
+		$query = $entity->query()->with(['store']);
+		$this->fillQueryForIndex($request, $query);
+		$count = $query->count();
+		$arr = explode(',', $sort);
+		//var_dump($arr);
+		foreach ($arr as $order) {
+			$tmpArr = explode(' ', trim($order));
+			$query->orderBy($tmpArr[0], $tmpArr[1]);
+		}
+		$data = $query->take($pageSize)->skip(($page - 1) * $pageSize)->get();
+		//LogSvr::apiSql()->info($query->toSql());
+		return response(['count' => $count, 'list' => $data, 'page' => $page, 'pageSize' => $pageSize], 200);
+	}
 }
