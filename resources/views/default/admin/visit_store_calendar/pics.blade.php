3a72f2dfa64fff6961de1d072b652bc71660e945
gsjohn <sz.gs.john@gmail.com>
2017-12-10 15:50:05 +0800

    bug fix

--- resources/views/default/admin/visit_store_calendar/pics.blade.php
+++ resources/views/default/admin/visit_store_calendar/pics.blade.php
@@ -11,7 +11,6 @@
     <div class="box-body">
         <div class="row">
             <!-- Jssor Slider Begin -->
-
             <style>
                 /* jssor slider loading skin spin css */
                 .jssorl-009-spin img {
@@ -31,19 +30,19 @@
                     }
                 }
             </style>
-
-            <div id="slider1_container" style="visibility: hidden; position: relative; margin: 0 auto; width: 1140px; height: 442px; overflow: hidden;">
+            <div id="slider1_container" style="visibility: hidden; position: relative; margin: 0 auto;
+        top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden;">
                 <!-- Loading Screen -->
                 <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
-                    <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="/assets/plugins/jssor/svg/loading/static-svg/spin.svg" />
+                    <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="../svg/loading/static-svg/spin.svg" />
                 </div>
 
                 <!-- Slides Container -->
-                <div u="slides" style="position: absolute; left: 0px; top: 0px; width:600px; height: 600px; overflow: hidden;">
+                <div u="slides" style="position: absolute; left: 0px; top: 0px; width: 1300px; height: 500px; overflow: hidden;">
                     @foreach($ids as $id)
-                    <div>
-                        <img data-u="image" src="/admin/show-image?imageId={{$id}}" />
-                    </div>
+                        <div>
+                            <img data-u="image" src="/admin/show-image?imageId={{$id}}" />
+                        </div>
                     @endforeach
                 </div>
 
@@ -88,47 +87,47 @@
                 <!--#endregion Arrow Navigator Skin End -->
             </div>
             <!-- Jssor Slider End -->
-
         </div>
     </div>
 </div>
 
 <script>
-
     jQuery(document).ready(function ($) {
+
         var options = {
-            $AutoPlay: 1,                                       //[Optional] Auto play or not, to enable slideshow, this option must be set to greater than 0. Default value is 0. 0: no auto play, 1: continuously, 2: stop at last slide, 4: stop on click, 8: stop on user navigation (by arrow/bullet/thumbnail/drag/arrow key navigation)
-            $AutoPlaySteps: 1,                                  //[Optional] Steps to go for each navigation request (this options applys only when slideshow disabled), the default value is 1
-            $Idle: 2000,                                        //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+            $FillMode: 2,                                       //[Optional] The way to fill image in slide, 0 stretch, 1 contain (keep aspect ratio and put all inside slide), 2 cover (keep aspect ratio and cover whole slide), 4 actual size, 5 contain for large image, actual size for small image, default value is 0
+            $AutoPlay: 1,                                    //[Optional] Auto play or not, to enable slideshow, this option must be set to greater than 0. Default value is 0. 0: no auto play, 1: continuously, 2: stop at last slide, 4: stop on click, 8: stop on user navigation (by arrow/bullet/thumbnail/drag/arrow key navigation)
+            $Idle: 4000,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
             $PauseOnHover: 1,                                   //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, 4 freeze for desktop, 8 freeze for touch device, 12 freeze for desktop and touch device, default value is 1
 
             $ArrowKeyNavigation: 1,   			            //[Optional] Steps to go for each navigation request by pressing arrow key, default value is 1.
-            $SlideEasing: $Jease$.$OutQuint,                    //[Optional] Specifies easing for right to left animation, default value is $Jease$.$OutQuad
-            $SlideDuration: 800,                                //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+            $SlideEasing: $Jease$.$OutQuint,          //[Optional] Specifies easing for right to left animation, default value is $Jease$.$OutQuad
+            $SlideDuration: 800,                               //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
             $MinDragOffsetToSlide: 20,                          //[Optional] Minimum drag offset to trigger slide, default value is 20
             //$SlideWidth: 600,                                 //[Optional] Width of every slide in pixels, default value is width of 'slides' container
             //$SlideHeight: 300,                                //[Optional] Height of every slide in pixels, default value is height of 'slides' container
             $SlideSpacing: 0, 					                //[Optional] Space between each slide in pixels, default value is 0
-            $Cols: 1,                                           //[Optional] Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
+            $Cols: 1,                                  //[Optional] Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
             $Align: 0,                                //[Optional] The offset position to park slide (this options applys only when slideshow disabled), default value is 0.
             $UISearchMode: 1,                                   //[Optional] The way (0 parellel, 1 recursive, default value is 1) to search UI components (slides container, loading screen, navigator container, arrow navigator container, thumbnail navigator container etc).
             $PlayOrientation: 1,                                //[Optional] Orientation to play slide (for auto play, navigation), 1 horizental, 2 vertical, 5 horizental reverse, 6 vertical reverse, default value is 1
             $DragOrientation: 1,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $Cols is greater than 1, or parking position is not 0)
 
+            $BulletNavigatorOptions: {                          //[Optional] Options to specify and enable navigator or not
+                $Class: $JssorBulletNavigator$,                 //[Required] Class to create navigator instance
+                $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
+                $Rows: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
+                $SpacingX: 8,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
+                $SpacingY: 8,                                   //[Optional] Vertical space between each item in pixel, default value is 0
+                $Orientation: 1,                                //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+                $Scale: false                                   //Scales bullets navigator or not while slider scale
+            },
+
             $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
                 $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
                 $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
                 $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
-            },
-
-            $BulletNavigatorOptions: {                                //[Optional] Options to specify and enable navigator or not
-                $Class: $JssorBulletNavigator$,                       //[Required] Class to create navigator instance
-                $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-                $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
-                $Rows: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
-                $SpacingX: 12,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
-                $SpacingY: 4,                                   //[Optional] Vertical space between each item in pixel, default value is 0
-                $Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
             }
         };
 
@@ -137,10 +136,9 @@
         //responsive code begin
         //you can remove responsive code if you don't want the slider scales while window resizing
         function ScaleSlider() {
-            var parentWidth = jssor_slider1.$Elmt.parentNode.clientWidth;
-            if (parentWidth) {
-                jssor_slider1.$ScaleWidth(parentWidth - 30);
-            }
+            var bodyWidth = document.body.clientWidth;
+            if (bodyWidth)
+                jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920));
             else
                 window.setTimeout(ScaleSlider, 30);
         }
@@ -151,5 +149,4 @@
         $(window).bind("orientationchange", ScaleSlider);
         //responsive code end
     });
-
 </script>
