6527089b542420679303c72b85c39d7d4aa1adc6
gsjohn <sz.gs.john@gmail.com>
2017-12-13 10:06:54 +0800

    bug fix

---
+++ database/migrations/2017_12_13_100032_alter_view_sale_order_items.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterViewSaleOrderItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        $q = <<<EOD
+CREATE OR REPLACE VIEW view_sale_order_items AS  
+SELECT
+e.fname as employee_name,
+p.fname as position_name,
+o.fcreate_date,
+o.fdate,
+s.ffullname as store_name,
+s.fnumber as store_number,
+`f`.`fname` AS store_channel,
+c.id as customer_id, 
+c.fname as customer_name, 
+m.fname as material_name,
+i.id,
+i.box_qty,
+i.bottle_qty,
+i.present_box_qty,
+i.present_bottle_qty,
+i.famount,
+i.fsend_status,
+o.fsend_date
+
+from st_sale_order_items i
+INNER JOIN st_sale_orders o on i.fsale_order_id = o.id
+LEFT JOIN bd_employees e on o.femp_id = e.id
+LEFT JOIN bd_positions p on e.fpost_id = p.id
+LEFT JOIN st_stores s on o.fstore_id = s.id
+LEFT JOIN `bd_channels` `f` ON `s`.`fchannel` = `f`.`id`
+LEFT JOIN bd_customers c on o.fcust_id = c.id
+LEFT JOIN bd_materials m on i.fmaterial_id = m.id
+
+EOD;
+
+        DB::statement($q);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
